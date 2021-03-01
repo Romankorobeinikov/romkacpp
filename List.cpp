94689e9cd2f9816a1b875afe7895d6c81dc662b1
Romankorobeinikov <korobeinikov.rv@phystech.edu>
2021-03-01 14:15:55 +0300

    Bag fix 1

--- List.cpp
+++ List.cpp
@@ -71,7 +71,6 @@ List copyList(List list)
 	}
 	copy = addEnd(copy, cell.data);
 	}
-	delete *cell;
 	return copy;
 }
 
@@ -91,26 +90,18 @@ void printList(List list)
 		}
 		cout << cell.data << endl;
 	}
-	delete *cell;
+	cout << endl;
 	return;
 }
 
 List mergeLists(List first_list, List second_list)
 {
-	Cell cell;
-	List first_list_copy = first_list;
 	if (second_list.length != 0)
 	{
-		cell = *second_list.first;
-		while (cell.next_ptr != nullptr)
-		{
-			first_list_copy = addEnd(first_list_copy, cell.data);
-			cell = *cell.next_ptr;
-		}
-		first_list_copy = addEnd(first_list, cell.data);
+		first_list.last->next_ptr = second_list.first;
 	}
-	delete *cell;
-	return first_list_copy;
+    first_list.length += second_list.length;
+	return first_list;
 }
 
 List reverseList(List list)
@@ -129,7 +120,6 @@ List reverseList(List list)
 	}
 	reverselist = addBegin(reverselist, cell.data);
 	}
-	delete *cell;
 	return reverselist;
 }
 
@@ -141,8 +131,7 @@ List del_i(List list, int i)
 
 	if (i == 1)
 	{
-		list.length = list.length - 1;
-		delete list.first;
+		list.length -= 1;
 		list.first = list.first->next_ptr;
 	}
 	else if ( i <= list.length && i > 1)
@@ -155,10 +144,9 @@ List del_i(List list, int i)
 			cell = *cell.next_ptr;
 		}
 		previous_cell.next_ptr = cell.next_ptr;
-		delete *cell;
-		return list;
-	}
-	delete *cell;
+	} else {
+        cout << "Can not do this operation" << endl;
+        }
 	return list;
 }
 
@@ -167,23 +155,23 @@ List add_i(List list, int data, int i)
 {
 	Cell cell;
 	Cell *new_cell = new Cell;
-	new_cell.data = data;
+	new_cell->data = data;
 	if (i == 1)
 	{
-		list.length = list.length + 1;
-		new_cell.next_ptr = list.first;
-		list.first = *new_cell;
+		list.length += 1;
+		new_cell->next_ptr = list.first;
+		list.first = new_cell;
 		return list;
 	}
 	else if ((list.length != 0) && (list.length >= i))
 	{
 		list.length = list.length + 1;
-		cell = list.first;
+		cell = *list.first;
 		for (int j = 0; j < i; j++)
 		{
 			cell = *cell.next_ptr;
 		}
-		new_cell.next_ptr = cell.next_ptr;
+		new_cell->next_ptr = cell.next_ptr;
 		cell.next_ptr = new_cell;
 		return list;
 	}
@@ -192,6 +180,7 @@ List add_i(List list, int data, int i)
 		cout << "В списке нет "<< i << "-ого элемента" << endl;
 		return list;
 	}
+
 }
 
 int main()
@@ -203,17 +192,22 @@ int main()
 	list = addEnd(list, 3);
 	list = addEnd(list, 5);
 	list = addEnd(list, 6);
+	list = addBegin(list, 2);
+	printList(list);
 	list = add_i(list, 4, 4);
+	printList(list);
 	second_list = createEmpty();
 	second_list = addBegin(second_list, 4);
 	second_list = addEnd(second_list, 10);
 	second_list = addBegin(second_list, 1);
 	second_list = addEnd(second_list, 12);
 	second_list = addBegin(second_list, 19);
+	printList(second_list);
 	new_list = mergeLists(list, second_list);
-	new_list = del_i(new_list, 8);
+	printList(list);
+	List newl = add_i(new_list, 36, 1);
 	printList(new_list);
-	copy = copyList(new_list);
-	printList(copy);
-	printList(reverseList(copy));
+	copy = copyList(newl);
+	//printList(copy);
+	//printList(reverseList(copy));
 }
